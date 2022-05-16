74a550e24afb367febcf6f9f79dfca0fe2841116
Jason Collum <jasoncollum@gmail.com>
2022-05-16 14:05:41 -0500

    initial commit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,170 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Email:</label>
+    <input type="email" required v-model="email"/>
+
+    <label>Password:</label>
+    <input type="password" required v-model="password"/>
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="web-developer">Web Developer</option>
+      <option value="web-designer">Web Designer</option>
+    </select>
+
+    <!-- if checked, added to names array -->
+    <div>
+      <input type="checkbox" value="sydney" v-model="names" />
+      <label>Sydney</label>
+    </div>
+    <div>
+      <input type="checkbox" value="bertie" v-model="names" />
+      <label>Bertie</label>
+    </div>
+    <div>
+      <input type="checkbox" value="jeep" v-model="names" />
+      <label>Jeep</label>
+    </div>
+
+    <label>Skills (Press Down Arrow To Add Each Skill)</label>
+    <input type="text" v-model="tempSkill" @keyup.down="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill" @click="deleteSkill(skill)">
+      {{ skill }}
+    </div>
+
+    <div class="terms">
+      <input type="checkbox" required v-model="terms" />
+      <label>Accept Terms and Conditions</label>
+    </div>
+
+    <div class="submit">
+      <button >Create An Account</button>
+    </div>
+
+  </form>
+
+  <p>Email:: {{ email }}</p>
+  <p>Password:: {{ password }}</p>
+  <p>Role:: {{ role }}</p>
+  <p>Terms Accepted:: {{ terms }}</p>
+  <p>Names:: {{ names }}</p>
+  <p>Temp Skill Value:: {{ tempSkill }}</p>
+  <p>Skills Array:: {{ skills }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: '',
+      terms: false,
+      names: [],
+      tempSkill: '',
+      skills: [],
+      passwordError: ''
+    }
+  },
+  methods: {
+    addSkill() {
+      if (this.tempSkill && !this.skills.includes(this.tempSkill)) {
+        this.skills.push(this.tempSkill)
+      }
+      this.tempSkill = ''
+    },
+    deleteSkill(s) {
+      this.skills = this.skills.filter(skill => skill !== s)
+    },
+    handleSubmit() {
+      this.passwordError = this.password.length > 5 ? 
+      '' : 'Password must be at leat 6 chars long'
+
+      if (!this.passwordError) {
+        console.log({
+        email: this.email,
+        password: this.password,
+        role: this.role,
+        names: this.names,
+        skills: this.skills
+        })
+        this.email = ''
+        this.password = ''
+        this.role = ''
+        this.terms = false
+        this.names = []
+        this.skills = []
+      }
+      
+    }
+  }
+}
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input, select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+  outline: none;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+  cursor: pointer;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
+</style>
